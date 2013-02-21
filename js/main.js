78ae96e73da382aef6af196b416d3e2d092e3673
Joe Madden <joe@beardbro.com>
2013-02-21 10:47:12 -0500

    pushing first round of pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+$(document).ready(function() {
 
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    var viewport = document.querySelector("meta[name=viewport]");
+
+    if ( width > 640 ) {
+        viewport.setAttribute('content', 'width=1024; initial-scale=1.0;');
+    }
+
+});
